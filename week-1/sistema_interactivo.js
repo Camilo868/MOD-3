18de93a0a1261641aae02fc1ea6b20427a61fa53
Camilo868 <camiloramirezosorio12345@gmail.com>
2026-05-09 21:45:09 -0500

    Task-3: validate age

--- week-1/sistema_interactivo.js
+++ week-1/sistema_interactivo.js
@@ -1,2 +1,7 @@
 const nombre = prompt("Enter your name:");
 const edad = prompt("Enter your age:");
+if (isNaN(edad)) {
+    alert("Please enter a valid number for age.");
+}else {
+    alert(`Hello, ${nombre}! You are ${edad} years old.`);
+}
