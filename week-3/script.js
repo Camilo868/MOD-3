a29b6ecf4e8bbfb5bf13985992608b7a3f5d7ddb
Camilo868 <camiloramirezosorio12345@gmail.com>
2026-05-21 22:22:45 -0500

    Task 2:Selección e inspección

---
+++ week-3/script.js
@@ -0,0 +1,10 @@
+
+const inputNota = document.getElementById('inputNota');
+
+const btnAgregar = document.querySelector('button'); 
+
+const listaNotas = document.getElementById('listaNotas');
+
+ console.log('Referencia del input:', inputNota);
+console.log('Referencia del botón:', btnAgregar);
+console.log('Referencia de la lista <ul>:', listaNotas);
